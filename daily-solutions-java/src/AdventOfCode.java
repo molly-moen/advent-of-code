7a23f19ace7b2be9628ed8c25a9c97d746496785
molly-moen <molly@code.org>
2021-12-03 09:08:23 -0800

    day 2

--- daily-solutions-java/src/AdventOfCode.java
+++ daily-solutions-java/src/AdventOfCode.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 public class AdventOfCode {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println(Day1.puzzle2());
+		System.out.println(Day2.puzzle2(readInFileLines("day2-input.txt")));
 	}
 
 	
 	public static ArrayList<String> readInFileLines(String filename) throws FileNotFoundException {
 		ArrayList<String> result = new ArrayList<String>();
-		System.out.println(new File(".").getAbsolutePath());
-		Scanner input = new Scanner(new File(filename));
+		Scanner input = new Scanner(new File("src/inputs/" + filename));
 		while(input.hasNextLine()) {
 			result.add(input.nextLine());
 		}
