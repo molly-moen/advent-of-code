a829fc0ee2d10ed87efd645ffdc61ac8e8b5eefa
molly-moen <molly@code.org>
2022-11-30 10:46:52 -0800

    initial setup

---
+++ 2022-typescript/src/day1.ts
@@ -0,0 +1,5 @@
+
+export const day1 = () => {
+  let message: string ='Hello World';
+  console.log(message);
+}
---
+++ 2022-typescript/src/run.ts
@@ -0,0 +1,9 @@
+import { day1 } from "./day1";
+
+class Run {
+  public static start() {
+    day1();
+  }
+}
+
+Run.start();
